6f4872894513ad637f918890da603fcc66f0b081
Joel <joel_li@yeah.net>
2022-06-15 21:24:56 +0800

    support use windows system clipboard

--- install.sh
+++ install.sh
@@ -7,3 +7,9 @@ sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.
 # for c++ cmake
 sudo apt-get install zlib1g zlib1g-dev
 sudo apt-get install python3.8-venv
+
+# configure the clipboard, use it with windows system
+curl -sLo/tmp/win32yank.zip https://github.com/equalsraf/win32yank/releases/download/v0.0.4/win32yank-x64.zip
+unzip -p /tmp/win32yank.zip win32yank.exe > /tmp/win32yank.exe
+chmod +x /tmp/win32yank.exe
+sudo mv /tmp/win32yank.exe /usr/local/bin/
