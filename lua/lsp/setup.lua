709984540727a3186cd978a0dcb50302671a6ae9
Joel <35829084+Dudoll@users.noreply.github.com>
2023-11-21 19:17:50 +0800

    Update setup.lua

--- lua/lsp/setup.lua
+++ lua/lsp/setup.lua
@@ -1,36 +1,35 @@
 -- :h mason-default-settings
-require("mason").setup({
-  ui = {
-    icons = {
-      package_installed = "✓",
-      package_pending = "➜",
-      package_uninstalled = "✗",
-    },
-  },
-})
+-- require("mason").setup({
+--   ui = {
+--     icons = {
+--       package_installed = "✓",
+--       package_pending = "➜",
+--       package_uninstalled = "✗",
+--     },
+--   },
+-- })
 
 -- mason-lspconfig uses the `lspconfig` server names in the APIs it exposes - not `mason.nvim` package names
 -- https://github.com/williamboman/mason-lspconfig.nvim/blob/main/doc/server-mapping.md
-require("mason-lspconfig").setup({
-  -- 确保安装，根据需要填写
-    ensure_installed = {
-        "clangd", 
-    },
-})
+-- require("mason-lspconfig").setup({
+--   -- 确保安装，根据需要填写
+--     ensure_installed = {
+--         "clangd", "cmake"
+--     },
+-- })
 
-require("mason-lspconfig").setup_handlers {
-        -- The first entry (without a key) will be the default handler
-        -- and will be called for each installed server that doesn't have
-        -- a dedicated handler.
-        function (server_name) -- default handler (optional)
-            require("lspconfig")[server_name].setup {}
-        end,
-        -- Next, you can provide a dedicated handler for specific servers.
-        -- For example, a handler override for the `rust_analyzer`:
-        -- ["rust_analyzer"] = function ()
-            -- require("rust-tools").setup {}
-        -- end
-}
-
--- require("lspconfig").clangd.setup{}
-
+-- require("mason-lspconfig").setup_handlers {
+--         -- The first entry (without a key) will be the default handler
+--         -- and will be called for each installed server that doesn't have
+--         -- a dedicated handler.
+--         function (server_name) -- default handler (optional)
+--             require("lspconfig")[server_name].setup {}
+--         end,
+--         -- Next, you can provide a dedicated handler for specific servers.
+--         -- For example, a handler override for the `rust_analyzer`:
+--         -- ["rust_analyzer"] = function ()
+--             -- require("rust-tools").setup {}
+--         -- end
+-- }
+-- 部分网络无法通过 mason 来配置 server，所以直接采用手工配置的方式
+require("lspconfig").clangd.setup{}
